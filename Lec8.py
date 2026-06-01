bf7e7509a512bb9885de94b4eac6072f948e3b12
Sumaira Ramzan <sumaira.ramzan244@gmail.com>
2026-06-01 11:57:20 +0500

    Add files via upload

---
+++ Lec8.py
@@ -0,0 +1,17 @@
+
+# Strings in Python are immutable, which means that once a string is created, it cannot be changed. However, you can perform various operations on strings, such as concatenation, slicing, and indexing.
+# Here are some examples of string operations in Python:
+intro="AOA"
+name="Sumaira"
+
+print(intro + ','+name + '!') # concatenation of strings
+
+print(name.upper())
+print(name.lower())
+print(len(name))
+print(name[0]) # indexing of string
+print(name[-1]) # backward , reverse of numbers
+print(name[-2])
+
+print(name[3:6]) # slicing of string
+print(name[:6]) # slicing from start to 6
